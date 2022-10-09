be0693ad40991ec830fb6769decc9ecedb509887
piyushagni5@gmail.com <piyushagni5@gmail.com>
2022-10-09 10:57:46 +0530

    CRUD part updated

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\Task\StoreRequest;
+use DB;
+use App\Models\Task;
+use App\Http\Requests\Task\UpdateRequest;
+use Illuminate\Http\RedirectResponse;
+
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::orderBy('is_completed')
+            ->orderBy('id')
+            ->get();
+
+        return view('task.index')->with(compact('tasks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('task.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRequest $request)
+    {
+        $task = DB::transaction(fn() => Task::create($request->validated()));
+
+        return to_route('tasks.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Task $task)
+    {
+        return view('task.show')->with(compact('task'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Task $task)
+    {
+        return view('task.edit')->with(compact('task'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateRequest $request, Task $task)
+    {
+        DB::transaction(fn() => $task->update($request->validated()));
+
+        return to_route('tasks.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        DB::transaction(fn() => $task->delete());
+
+        return to_route('tasks.index');
+    }
+
+    public function complete(Task $task): RedirectResponse
+    {
+        DB::transaction(fn() => $task->update(['is_completed' => true]));
+        dd($task);
+
+        return to_route('tasks.index');
+    }
+
+    public function yetComplete(Task $task): RedirectResponse
+    {
+        DB::transaction(fn() => $task->update(['is_completed' => false]));
+
+        return to_route('tasks.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('tasks/create', [\App\Http\Controllers\TaskController::class, 'create'])->name('tasks.create');
+// Route::post('tasks', [\App\Http\Controllers\TaskController::class, 'store'])->name('tasks.store');
+// Route::get('tasks', [\App\Http\Controllers\TaskController::class, 'index'])->name('tasks.index');
+// Route::get('tasks/{task}', [\App\Http\Controllers\TaskController::class, 'show'])->name('tasks.show');
+// Route::get('tasks/{task}/edit', [\App\Http\Controllers\TaskController::class, 'edit'])->name('tasks.edit');
+// // Route::put('tasks/{task}', [\App\Http\Controllers\TaskController::class, 'update'])->name('tasks.update');
+// Route::patch('tasks/{task}/complete', [\App\Http\Controllers\TaskController::class, 'complete'])->name('tasks.complete');
+// Route::patch('tasks/{task}/yet_complete', [\App\Http\Controllers\TaskController::class, 'yetComplete'])->name('tasks.yet_complete');
+
+Route::controller(\App\Http\Controllers\TaskController::class)
+    ->prefix('tasks/{task}')
+    ->as('tasks.')
+    ->group(function () {
+        Route::patch('complete', 'complete')->name('complete');
+        Route::patch('yet_complete', 'yetComplete')->name('yet_complete');
+    });
+
+Route::resource('tasks', TaskController::class);
